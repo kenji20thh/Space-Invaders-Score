6b76d6bb892b8d277dc23f6ef0a100f9fb1de6cc
kenji20thh <amineelayachi22@outlook.com>
2025-05-13 13:01:06 +0200

    Post-Request-Handlefunc

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
 	"sync"
 )
 
@@ -23,3 +25,29 @@ var (
 	scoresFile = "scores.json"
 	mutex      = &sync.Mutex{}
 )
+
+
+
+// func main () {
+// 	http.HandleFunc("/score", handlePostScore)
+// 	http.HandleFunc("/scores", handleGetScores)
+// }
+
+func handlePostScore(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	var entry ScoreEntry 
+	if err := json.NewDecoder(r.Body).Decode(&entry); err != nil {
+		http.Error(w, "Invalid Json", http.StatusBadRequest)
+		return
+	}
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	scores := loadScores()
+	scores = append(scores, entry)
+	saveScores(scores)
+
+} 
