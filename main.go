e5807f9449c39044a15d1e1126253ea56908e83e
kenji20thh <amineelayachi22@outlook.com>
2025-05-13 12:20:59 +0200

    backend-global-variables

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+
+type ScoreEntry struct {
+	Name  string `json:"name"`
+	Score int    `json:"score"`
+	Time  string `json:"time"`
+}
+
+
+type RankedScore struct {
+	Name     string `json:"name"`
+	Score    int    `json:"score"`
+	Time     string `json:"time"`
+	Rank     int    `json:"rank"`
+	Percent  string `json:"percent,omitempty"`
+}
